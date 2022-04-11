7e879a2471c091a45a195d9c70f1ef2f6d374cbc
sh38p <sh38prog@gmail.com>
2022-04-11 21:12:23 +0900

    message add to heroes template

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +9,13 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = [];
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  selectedHero?: Hero;
 
+  heroes: Hero[] = [];
+  
+  selectedHero?: Hero;
+  
   constructor(
-    private heroService: HeroService) {
+    private heroService: HeroService, private messageService: MessageService) {
      }
 
   ngOnInit(): void {
@@ -26,6 +24,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`Herocomponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void{
